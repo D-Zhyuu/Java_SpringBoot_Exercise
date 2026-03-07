e76f76e7d009012f1dbb69eb0caaba4ad1e711f6
DingZiHeng <631573805@qq.com>
2026-03-07 16:06:13 +0800

    add BaseEntity and User class

--- store/src/main/java/com/cy/store/entity/BaseEntity.java
+++ store/src/main/java/com/cy/store/entity/BaseEntity.java
@@ -2,11 +2,70 @@ package com.cy.store.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 /* 作为实体类的基类 */
 public class BaseEntity implements Serializable {
-    private String created_user;
-    private Date created_time;
-    private String modified_user;
-    private Date modified_time;
+    private String createdUser;
+    private Date createdTime;
+    private String modifiedUser;
+    private Date modifiedTime;
+
+    public String getCreatedUser() {
+        return createdUser;
+    }
+
+    public void setCreatedUser(String createdUser) {
+        this.createdUser = createdUser;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getModifiedUser() {
+        return modifiedUser;
+    }
+
+    public void setModifiedUser(String modifiedUser) {
+        this.modifiedUser = modifiedUser;
+    }
+
+    public Date getModifiedTime() {
+        return modifiedTime;
+    }
+
+    public void setModifiedTime(Date modifiedTime) {
+        this.modifiedTime = modifiedTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof BaseEntity that)) return false;
+
+        return Objects.equals(getCreatedUser(), that.getCreatedUser()) && Objects.equals(getCreatedTime(), that.getCreatedTime()) && Objects.equals(getModifiedUser(), that.getModifiedUser()) && Objects.equals(getModifiedTime(), that.getModifiedTime());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(getCreatedUser());
+        result = 31 * result + Objects.hashCode(getCreatedTime());
+        result = 31 * result + Objects.hashCode(getModifiedUser());
+        result = 31 * result + Objects.hashCode(getModifiedTime());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "createdUser='" + createdUser + '\'' +
+                ", createdTime=" + createdTime +
+                ", modifiedUser='" + modifiedUser + '\'' +
+                ", modifiedTime=" + modifiedTime +
+                '}';
+    }
 }
